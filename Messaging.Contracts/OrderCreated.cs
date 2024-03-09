a6a7b0102584cd04c6452a6c5cad3dc1762a49d4
Duy Phan <phanjuy@hotmail.com>
2024-03-09 17:56:07 +0100

    Created Messaging.Contracts class library with OrderCreated event model

---
+++ Messaging.Contracts/OrderCreated.cs
@@ -0,0 +1,10 @@
+﻿namespace Messaging.Contracts;
+
+// Event message type
+public class OrderCreated
+{
+    int Id { get; set; }
+    string ProductName { get; set; } = string.Empty;
+    decimal Price { get; set; }
+    int Quantity { get; set; }
+}
