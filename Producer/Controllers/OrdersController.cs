e6b30fc5ab728b21d2b7d9ddfc4db516397a7647
Duy Phan <phanjuy@hotmail.com>
2024-03-09 18:11:48 +0100

    Create OrdersController

---
+++ Producer/Controllers/OrdersController.cs
@@ -0,0 +1,31 @@
+﻿using MassTransit;
+using Messaging.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Producer.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OrdersController : ControllerBase
+{
+    private readonly IPublishEndpoint _publishEndpoint;
+
+    public OrdersController(IPublishEndpoint publishEndpoint)
+    {
+        _publishEndpoint = publishEndpoint;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateOrder(OrderDto orderDto)
+    {
+        await _publishEndpoint.Publish<OrderCreated>(new
+        {
+            Id = 1,
+            orderDto.ProductName,
+            orderDto.Quantity,
+            orderDto.Price
+        });
+
+        return Ok();
+    }
+}
