675f9c40aa92f68e7ceb6b32a7067916d439685a
Duy Phan <phanjuy@hotmail.com>
2024-03-09 19:56:25 +0100

    Add consumer

---
+++ Consumer/OrderCreatedConsumer.cs
@@ -0,0 +1,15 @@
+﻿using MassTransit;
+using Messaging.Contracts;
+using System.Text.Json;
+
+namespace Consumer;
+
+internal class OrderCreatedConsumer : IConsumer<OrderCreated>
+{
+    public Task Consume(ConsumeContext<OrderCreated> context)
+    {
+        var jsonMessage = JsonSerializer.Serialize(context.Message);
+        Console.WriteLine($"OrderCreated message: {jsonMessage}");
+        return Task.CompletedTask;
+    }
+}
---
+++ Consumer/Program.cs
@@ -0,0 +1,24 @@
+﻿using Consumer;
+using MassTransit;
+
+IBusControl busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
+{
+    cfg.ReceiveEndpoint("order-created-event", e =>
+    {
+        e.Consumer<OrderCreatedConsumer>();
+    });
+
+});
+
+await busControl.StartAsync(new CancellationToken());
+
+try
+{
+    Console.WriteLine("Press enter to exit");
+
+    await Task.Run(() => Console.ReadLine());
+}
+finally
+{
+    await busControl.StopAsync();
+}
